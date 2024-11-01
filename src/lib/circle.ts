740e05631a3af8c13aece1bded21127ee96ccbdf
Zarif Karim <zarifkarim09@gmail.com>
2024-11-01 21:03:49 +1100

    update circle class

--- src/lib/circle.ts
+++ src/lib/circle.ts
@@ -6,20 +6,42 @@ export type CircleOptions = {
 
 export class Circle extends Container {
   public view: Graphics;
-  public radius: number;
-  public color: ColorSource;
+  public _radius: number;
+  public _color: ColorSource;
 
   constructor(x: number, y: number, radius: number, options?: CircleOptions) {
     super();
-    this.color = options?.color || "pink";
-    this.view = new Graphics()
-      .circle(0, 0, radius)
-      .fill({ color: options?.color || "pink" });
+    this._color = options?.color || "pink";
+    this._radius = radius;
+
+    this.view = new Graphics();
+    this.drawCircle();
     this.addChild(this.view);
 
+    // settin size and pos of the container
     this.position.set(x, y);
     this.setSize(radius, radius);
+  }
 
-    this.radius = radius;
+  private drawCircle() {
+    this.view.clear().circle(0, 0, this._radius).fill({ color: this._color });
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set color(newColor: ColorSource) {
+    this._color = newColor;
+    this.drawCircle();
+  }
+
+  get radius() {
+    return this._radius;
+  }
+
+  set radius(radius: number) {
+    this._radius = radius;
+    this.drawCircle();
   }
 }
