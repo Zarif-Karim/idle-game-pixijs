59309cbeeb8df67ac22c195e0f19dca0a7009d59
Zarif Karim <zarifkarim09@gmail.com>
2024-11-08 20:05:15 +1100

    test resolution update

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,21 @@ import "./style.css";
 import { Application } from "pixi.js";
 import start from "./app";
 import { addFullScreenButton } from "./screen-resize";
+import { EDGES } from "./globals";
 
 (async () => {
   const app = new Application();
-  await app.init({ resizeTo: window });
+  await app.init({
+    resizeTo: window,
+    resolution: window.devicePixelRatio || 1,
+    width: EDGES.width,
+    height: EDGES.height,
+    autoDensity: true,
+  });
   const root = document.querySelector<HTMLDivElement>("#app")!;
   root.appendChild(app.canvas);
 
+  console.log();
   await addFullScreenButton(app);
 
   start(app);
